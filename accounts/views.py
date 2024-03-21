9063dd5a6736eea4133939a2d60a8edd8236f0e5
joblackpoc <duopoc@gmail.com>
2024-03-21 23:04:27 +0700

    check register and login

--- accounts/views.py
+++ accounts/views.py
@@ -12,12 +12,22 @@ def register(request):
         password = request.POST['password']
         password2 = request.POST['password2']
 
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email, password=password)
-        user.save()
-        messages.success(request, 'You are now registered and can log in')
-        return redirect('login')
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already exists, choose another one')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already exists, choose another one')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email, password=password)
+                    user.save()
+                    messages.success(request, 'You are now registered and can log in')
+                    return redirect('login')
+        else:
+            return redirect('register') 
     else:
-        messages.error(request, 'Passwords do not match')
         return render(request, 'accounts/register.html')
 
 def login(request):
