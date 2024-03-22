4661797fb6db4245b7cabe017d1352e4ad0604f9
joblackpoc <duopoc@gmail.com>
2024-03-22 21:27:22 +0700

    Update settings.py

--- PYSTUDY/settings.py
+++ PYSTUDY/settings.py
@@ -146,7 +146,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'djangonatthawut@gmail.com'
-EMAIL_HOST_PASSWORD = 'bzzidiuabhchlcfx'
+EMAIL_HOST_PASSWORD = ''
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_FILE_PATH = BASE_DIR, 'sent_emails'
