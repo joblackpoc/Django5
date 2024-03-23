331e94eafc5ce55faf7237d0e6c8d731d2a3bdbb
joblackpoc <duopoc@gmail.com>
2024-03-23 23:54:47 +0700

    password reset complete

--- PYSTUDY/settings.py
+++ PYSTUDY/settings.py
@@ -25,9 +25,9 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
